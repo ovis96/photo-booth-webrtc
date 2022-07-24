f4c1a138832ea1c322ca622d7320a18aa7f282f9
Ovishek Paul <ovishek.paul@gmail.com>
2022-07-24 10:58:04 +0600

    take photo

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import "./App.css";
 import Camera from "./components/Camera";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Camera />
--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -3,16 +3,54 @@ import { getVideoCamera } from "../webrtc.utils";
 
 const Camera = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [images, setImages] = useState<string[]>([]);
+
   useEffect(() => {
     getVideoCamera().then((stream) => {
       try {
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
+          setMediaStream(stream);
         }
       } catch {}
     });
   }, []);
-  return <video ref={videoRef} autoPlay muted />;
+
+  const onTakePhoto = async () => {
+    const track = mediaStream?.getVideoTracks()[0];
+    // @ts-ignore
+    const photo = new window.ImageCapture(track);
+    const blob = await photo.takePhoto();
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    const base64Image = await new Promise((resolve) => {
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+    });
+    setImages((images) => [...images, base64Image as string]);
+  };
+
+  console.log("~~~ images", images);
+
+  return (
+    <div>
+      <div>
+        <video ref={videoRef} autoPlay muted />
+        <div>
+          <button onClick={onTakePhoto}> Take Photo </button>
+        </div>
+      </div>
+
+      <div>
+        {images.map((image, index) => {
+          return <img key={index} src={image} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Camera;
